d336e3dcf275863e66fe46cb2ea874377dcafc6e
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-03-06 22:50:33 +0300

    component Likes

---
+++ src/Likes.js
@@ -0,0 +1,35 @@
+import { connect } from "react-redux";
+
+function Likes(props) {
+    console.log(props);
+    return (
+        <div className="button-controls">
+            <button onClick={props.onIncrementLikes} >❤ {props.likes}</button>
+            <button onClick={props.onDecrementLikes}>Dislike</button>
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    console.log('mapStateToProps >', state)
+    return {
+        likes: state.likes
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onIncrementLikes: () => {
+            console.log('click');
+            const action = { type: 'INCREMENT' }
+            dispatch(action)
+        },
+        onDecrementLikes: () => {
+            console.log('click decrement');
+            const action = { type: 'DECREMENT' }
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes);
